b1985095e7edc9928e695920bce2698a7ca6401d
MrHexeberg <marhex@hotmail.com>
2024-10-17 17:32:51 +0200

    start note + test
    test starte

--- AI_Model_Pill/Data_Prosessing.py
+++ AI_Model_Pill/Data_Prosessing.py
@@ -1,3 +1,10 @@
+#what do we need to do to the data
+#what can be smart to do
+# 1 . normalice tablet hardness and hight
+# 2 clean up the pressforce
+# 3 do tomting about the partical size data
+
+
 import pandas as pd
 from sklearn.preprocessing import minmax_scale
 import matplotlib.pyplot as plt
---
+++ AI_Model_Pill/main.py
@@ -0,0 +1,5 @@
+#figger out what we need the model to do and
+# figger out what moddel would work best wiht this
+
+#hei dette er en test
+
